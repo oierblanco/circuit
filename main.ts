a18acbd1027f756abe00da8facf23c76e3ea85b3
oierblanco <93262346+oierblanco@users.noreply.github.com>
2022-05-25 14:29:49 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+DFRobotMaqueenPlus.I2CInit()
+let pared = DFRobotMaqueenPlus.ultraSonic(PIN.P0, PIN.P0)
+basic.forever(function () {
+    if (pared == 10) {
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+        basic.pause(250)
+    }
+})
+basic.forever(function () {
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 75)
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 75)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+        } else if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 75)
+        }
+    }
+})
