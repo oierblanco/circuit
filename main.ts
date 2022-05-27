6f06ee6a309e19470ebb6a8814571c13534f897f
oierblanco <93262346+oierblanco@users.noreply.github.com>
2022-05-27 14:25:20 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,42 @@
 DFRobotMaqueenPlus.I2CInit()
-let pared = DFRobotMaqueenPlus.ultraSonic(PIN.P0, PIN.P0)
+let _1_atala = 1
+let _2_atala = 0
+let _3_atala = 0
 basic.forever(function () {
-    if (pared == 10) {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
-        basic.pause(250)
+    if (_1_atala == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 75)
+    }
+    if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) == 15) {
+        _1_atala = 0
+        _2_atala = 1
     }
 })
 basic.forever(function () {
-    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+    if (_2_atala == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+        basic.pause(800)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.pause(250)
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 75)
-        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 75)
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
-        } else if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 75)
+        basic.pause(500)
+        _2_atala = 0
+        _3_atala = 1
+    }
+})
+basic.forever(function () {
+    if (_3_atala == 1) {
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 75)
+        } else {
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 75)
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+            }
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
+                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 75)
+            }
         }
     }
 })
