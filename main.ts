50faa889bc0c187c331e96ba88f30e74d6c4f1f8
oierblanco <93262346+oierblanco@users.noreply.github.com>
2022-06-03 14:26:52 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ DFRobotMaqueenPlus.I2CInit()
 let _1_atala = 1
 let _2_atala = 0
 let _3_atala = 0
+let _4_atala = 0
 basic.forever(function () {
     if (_1_atala == 1) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
@@ -15,7 +16,7 @@ basic.forever(function () {
 basic.forever(function () {
     if (_2_atala == 1) {
         DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 60)
         basic.pause(800)
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
         basic.pause(250)
@@ -23,6 +24,28 @@ basic.forever(function () {
         basic.pause(500)
         _2_atala = 0
         _3_atala = 1
+        _4_atala = 1
+    }
+})
+basic.forever(function () {
+    if (_4_atala == 1) {
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.R3) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.L3) == 1 && (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1)) {
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 75)
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+            basic.pause(2000)
+            DFRobotMaqueenPlus.mototStop(Motors.ALL)
+            basic.pause(200)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.GREEN)
+            basic.pause(100)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.BLUE)
+            basic.pause(100)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW)
+            basic.pause(100)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.PINK)
+            basic.pause(100)
+            DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.CYAN)
+            _4_atala = 0
+        }
     }
 })
 basic.forever(function () {
